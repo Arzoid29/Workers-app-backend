a69aa450a9c073a037d374295b88a9073e47f167
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-04-13 10:03:34 -0400

    Solving some error in the upload

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
-const sequelize = new Sequelize('postgres::memory:');
+const sequelize = new Sequelize();
 
 const User = sequelize.define('public.users', {
   id: {
